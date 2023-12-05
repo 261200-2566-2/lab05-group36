1826d06de126ee95c12e90e5f93c9e4d388038c2
Mind <nathapat.mind@gmail.com>
2023-12-05 21:31:11 +0700

    final submit full score pls

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,25 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        // Create an RPGCharacter
+        Assassins hero = new Assassins("Hero", 1, 10.0);
+        hero.displayStat();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+//        FireRing fireRing = new FireRingImpl();
+//        BunnyShoes bunnyShoes = new BunnyShoesImpl();
+//        DiamondCloak diamondCloak = new DiamondCloakImpl();
+//
+//        hero.equipAccessories(fireRing);
+//        hero.equipAccessories(bunnyShoes);
+//        hero.equipAccessories(diamondCloak);
+
+
+        hero.displayStat();
+
+        Tanks hero2 = new Tanks("Gear", 2, 10.0);
+        hero2.displayStat();
+        hero.shadowDanceATK(hero2);
+        hero2.displayStat();
     }
 }
